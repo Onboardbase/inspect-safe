701456de396e9064c659494f4002e5139f399195
Mahmoud Galal <ssss.aaa2002@gmail.com>
2024-02-05 10:41:14 +0200

    chore: remove unused code

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,4 @@ import nudgeerSafe from './helpers/nudgeer-safe';
 /**
  * @module nudgeer-safe
  */
-(async()=>{console.log(await nudgeerSafe({framework:'NextJs',includeConfig:true}))})()
 module.exports = nudgeerSafe;
